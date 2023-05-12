81e7ce3b469a6c6666ad1bb9ba70842862da7158
luismomm <luismomm@gmail.com>
2023-05-11 21:38:29 -0300

    Adiciona teste para tempo

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,5 +1,5 @@
 from _decimal import Decimal
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -15,12 +15,14 @@ other_destinations = [Airport(code='SAN', coordinate=Coordinate(latitude=Decimal
                                                                 longitude=Decimal('-117.189657'))),
                       Airport(code='SFO', coordinate=Coordinate(latitude=Decimal('37.618972'),
                                                                 longitude=Decimal('-122.374889')))]
+default_date = datetime(2020, 1, 1)
+original_flight = Flight(source=source, destination=destination, departure=default_date, price=Decimal('100.00'))
 
 
 def test_returns_list_of_flights(fake_repository, fake_gateway):
     distance = 375000
 
-    flights = find_flights_within_range(source=source, destination=destination, departure=datetime(2020, 1, 1),
+    flights = find_flights_within_range(source=source, destination=destination, departure=default_date,
                                         desired_range=distance, repository=fake_repository, gateway=fake_gateway)
 
     assert len(flights) == 3
@@ -29,11 +31,19 @@ def test_returns_list_of_flights(fake_repository, fake_gateway):
 def test_when_no_there_is_no_airport_within_range_then_return_only_original_destination(fake_repository, fake_gateway):
     distance = 100
 
-    flights = find_flights_within_range(source=source, destination=destination, departure=datetime(2020, 1, 1),
+    flights = find_flights_within_range(source=source, destination=destination, departure=default_date,
                                         desired_range=distance, repository=fake_repository, gateway=fake_gateway)
 
-    assert flights == [Flight(source=source, destination=destination, departure=datetime(2020, 1, 1),
-                                price=Decimal('100.00'))]
+    assert flights == [original_flight]
+
+
+def test_when_search_for_a_date_without_flights_then_return_empty_list(fake_repository, fake_gateway):
+    distance = 375000
+
+    flights = find_flights_within_range(source=source, destination=destination, departure=default_date + timedelta(days=1),
+                                        desired_range=distance, repository=fake_repository, gateway=fake_gateway)
+
+    assert flights == []
 
 
 @pytest.fixture
@@ -44,9 +54,9 @@ def fake_repository():
 @pytest.fixture
 def fake_gateway():
     flights = [
-        Flight(source=source, destination=destination, departure=datetime(2020, 1, 1), price=Decimal('100.00')),
-        Flight(source=source, destination=other_destinations[0], departure=datetime(2020, 1, 1), price=Decimal('100.00')),
-        Flight(source=source, destination=other_destinations[1], departure=datetime(2020, 1, 1), price=Decimal('100.00')),
+        original_flight,
+        Flight(source=source, destination=other_destinations[0], departure=default_date, price=Decimal('100.00')),
+        Flight(source=source, destination=other_destinations[1], departure=default_date, price=Decimal('100.00')),
     ]
 
     return FakeGateway(flights=flights)
