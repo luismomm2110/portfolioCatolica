d6992c96386f0944cc711463e4e5ffbd867d0d4b
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-10-11 20:26:57 -0300

    [Refact] -  cria rota básica

---
+++ client/search_flights/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+import './App.css';
+import {PaginaInicial} from './PaginaInicial/PaginaInicial'
+
+const App = () => {
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <PaginaInicial/>
+        }
+    ]);
+
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+           <RouterProvider router={router} />
+        </React.StrictMode>
+    );
+}
+
+export default App;
