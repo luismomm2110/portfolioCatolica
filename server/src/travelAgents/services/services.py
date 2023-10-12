ef7c21591c3e66715c1dbf3509a8f9c19e032385
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-10-12 19:56:13 -0300

    [Refact] - retira string desnecessario

--- server/src/travelAgents/services/services.py
+++ server/src/travelAgents/services/services.py
@@ -9,14 +9,14 @@ class TravelAgentAlreadyExistsException(ValueError):
     pass
 
 
-def create_travel_agent(travel_agent_gateway: TravelAgentGateway, travel_agent: dict):
-    if travel_agent_gateway.get_travel_agent_by_email(travel_agent['email']):
+def create_travel_agent(travel_agent_gateway: TravelAgentGateway, travel_agent_data: dict):
+    if travel_agent_gateway.get_travel_agent_by_email(travel_agent_data['email']):
         raise TravelAgentAlreadyExistsException('Email already in use')
-    travel_agent['date_joined'] = datetime.now().isoformat()
-    travel_agent['_id'] = str(uuid.uuid4())
-    travel_agent['password'] = set_password(travel_agent['password'])
-    travel_agent = TravelAgent(**travel_agent)
-    travel_agent_gateway.save(travel_agent)
+    travel_agent_data['_id'] = uuid.uuid4()
+    travel_agent_data['password'] = set_password(travel_agent_data['password'])
+    travel_agent_data['date_joined'] = datetime.now().isoformat()
+    travel_agent_data = TravelAgent(**travel_agent_data)
+    travel_agent_gateway.save(travel_agent_data)
 
 
 def login_as_travel_agent(travel_agent_gateway: TravelAgentGateway, email: str, password: str):
