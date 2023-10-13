25981b30bb201e34c85b5047cc8c92992e88f360
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-10-13 11:43:17 -0300

    [Feature] - cria rotas novas

--- client/search_flights/src/App.tsx
+++ client/search_flights/src/App.tsx
@@ -5,15 +5,37 @@ import {
 } from "react-router-dom";
 import {PaginaInicial} from './PaginaInicial/PaginaInicial'
 import ErrorPage from "./ErrorPage/ErrorPage";
+import {useAuth} from "./auth/authProvider";
+import {ProtectedRoute} from "./auth/ProtectedRoute";
+
 
 const App = () => {
-    const router = createBrowserRouter([
+    const { token } = useAuth();
+
+    const routesForAuthenticatedUsers = [
+        {
+            path: '/',
+            element: <ProtectedRoute />,
+            children: [
+                {
+                    path: '/',
+                    element: <div>Authenticated</div>,
+                }
+            ]
+        }
+    ];
+
+    const routesForUnauthenticatedUsers = [
         {
             path: '/',
             element: <PaginaInicial/>,
             errorElement: <ErrorPage/>
         }
-    ]);
+    ];
+
+    const router = createBrowserRouter(
+        token ? routesForAuthenticatedUsers : routesForUnauthenticatedUsers
+    );
 
     return  <RouterProvider router={router} />
 }
