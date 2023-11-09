6da1f2b3ededa83029de4d79d24d1abed1f234b7
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-11-09 19:25:27 -0300

    [Feature] - gateway mongo db

--- server/src/FlightAreas/gateways/gateways.py
+++ server/src/FlightAreas/gateways/gateways.py
@@ -15,3 +15,13 @@ class FakeFlightAreaGateway(FlightAreaGateway):
 
     def get_flight_area_by_id(self, flight_area_id):
         return self._flight_areas[flight_area_id]
+
+
+class MongoFlightAreaGateway(FlightAreaGateway):
+    def __init__(self, mongo_client_with_collection):
+        self._mongo_client = mongo_client_with_collection
+
+    def save_flight_area(self, flight_area: FlightArea):
+        self._mongo_client.db.collection.insert_one(flight_area.to_dict())
+
+
--- server/src/FlightAreas/models/model.py
+++ server/src/FlightAreas/models/model.py
@@ -8,3 +8,12 @@ class FlightArea:
     name: str
     city_origin: str
     airports: list[str]
+
+    def to_dict(self):
+        return {
+            '_id': self._id,
+            'travel_agent_id': self.travel_agent_id,
+            'name': self.name,
+            'city_origin': self.city_origin,
+            'airports': self.airports
+        }
