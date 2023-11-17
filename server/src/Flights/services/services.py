ca47017084c445f7cc9fd279fc983d43da110e24
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-11-16 22:22:30 -0300

    refact

--- server/src/Flights/services/services.py
+++ server/src/Flights/services/services.py
@@ -11,37 +11,27 @@ from server.src.Flights.models.model import TripGoal
 def find_all_flights_from_airports(city_source: str, iata_airports_destinations: list[str], departure: str,
                                    airport_repository: AbstractRepository, flight_gateway: AbstractGateway,
                                    max_price: Optional[Decimal] = None, currency_rate_mapping: CurrencyRateMapping = None) -> [List[TripGoal], str]:
-    try:
-        airport_from_the_source = airport_repository.fetch_airports_by_municipality(city_source)[0]
-    except IndexError:
-        return [], 'City not found'
 
-    if not city_source or not airport_from_the_source:
+    airport_from_the_source = next(iter(airport_repository.fetch_airports_by_municipality(city_source)), None)
+    if not airport_from_the_source:
         return [], 'City not found'
 
     if invalid_date_message := _verify_if_departure_is_in_past(departure):
         return [], invalid_date_message
 
-    currencies_mapping = _get_currencies_mapping(currency_rate_mapping)
-    max_price_converted_to_euros = _get_max_price_converted_to_euros(currencies_mapping, max_price)
+    max_price_converted_to_euros = _get_max_price_converted_to_euros(currency_rate_mapping, max_price)
 
     flights = flight_gateway.get(airport_from_the_source.code,
                                  iata_airports_destinations,
                                  departure,
                                  max_price_converted_to_euros)
 
-    return _present_flights(flights, currencies_mapping), ''
+    return _present_flights(flights, currency_rate_mapping), ''
 
 
 def _get_max_price_converted_to_euros(currencies_mapping: CurrencyRateMapping, max_price):
     max_price_converted_to_euros = max_price / currencies_mapping.mapping.get('EUR') if max_price else None
-    return int(max_price_converted_to_euros)
-
-
-def _get_currencies_mapping(currency_rate):
-    if currency_rate is None:
-        currency_rate = dict()
-    return currency_rate
+    return int(max_price_converted_to_euros) if max_price_converted_to_euros else None
 
 
 def _verify_if_departure_is_in_past(date: str):
