fee9affc60d385bf5f5e26c52146805d60b5f4e9
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-11-26 21:27:14 -0300

    [feat] - melhoria da api de moeda

--- server/src/flight_search/CurrencyRate/gateways/gateways.py
+++ server/src/flight_search/CurrencyRate/gateways/gateways.py
@@ -1,7 +1,9 @@
+import requests
 from abc import abstractmethod, ABC
 from decimal import Decimal
 
 from flight_search.CurrencyRate.models.models import BRLCurrencyRateMapping, CurrencyRate
+from settings import get_currency_api_key
 
 
 class AbstractCurrencyRateGateway(ABC):
@@ -20,3 +22,22 @@ class FakeCurrencyRateGateway(AbstractCurrencyRateGateway):
         }
 
         return BRLCurrencyRateMapping(mapping=mapping)
+
+
+class ProductionCurrencyRateGateway(AbstractCurrencyRateGateway):
+    def get_currency_rate_mapping(self) -> BRLCurrencyRateMapping:
+        api_key = get_currency_api_key()
+        url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/BRL'
+        response = requests.get(url)
+        data = response.json()
+        mapping = {
+            currency: Decimal(str(rate))
+            for currency, rate in data['conversion_rates'].items()
+        }
+
+        return BRLCurrencyRateMapping(mapping=mapping)
+
+
+if __name__ == '__main__':
+    gateway = ProductionCurrencyRateGateway()
+    print(gateway.get_currency_rate_mapping())
