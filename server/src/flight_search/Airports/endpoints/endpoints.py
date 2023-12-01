c756ee5b0ceab9775289bdc1d3a97068c7b1ca32
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-12-01 15:29:27 -0300

    mongorepo

--- server/src/flight_search/Airports/endpoints/endpoints.py
+++ server/src/flight_search/Airports/endpoints/endpoints.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import jwt_required
+from pymongo import MongoClient
 
-from settings import get_jwt_key
-from flight_search.Airports.repositories.repositories import IataAirportRepository
+from flight_search.TravelAgents.gateways.gateways import MongoTravelAgentGateway
+from settings import get_jwt_key, get_mongo_url
+from flight_search.Airports.repositories.repositories import IataAirportRepository, MongoAirportRepository
 from flight_search.Airports.services.services import find_nearest_airports_by_city, find_city
 
 app = Flask(__name__)
 CORS(app)
 
+mongo_client = MongoClient(get_mongo_url(), 27017)
+
 app.config['SECRET_KEY'] = get_jwt_key()
+airport_client = mongo_client['search_flight_db']['travel_agent']
+repository = MongoAirportRepository(airport_client)
 
 
 @jwt_required
@@ -17,7 +23,6 @@ app.config['SECRET_KEY'] = get_jwt_key()
 def search_airports():
     city = request.args.get('city', type=str)
     limit = request.args.get('limit', default=10, type=int)
-    repository = IataAirportRepository()
 
     airports = find_nearest_airports_by_city(city, limit, repository)
     if not airports:
