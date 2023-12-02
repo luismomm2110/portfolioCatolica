40e6d34a7bff830b0258fbaf79c02b6984d685e0
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-12-02 11:52:30 -0300

    altera path

--- server/src/flight_search/app.py
+++ server/src/flight_search/app.py
@@ -10,31 +10,34 @@ CORS(app)
 
 print('Starting server...')
 
-@app.route('/flights', methods=['GET'])
+
+@app.route('/api/flights', methods=['GET'])
 def flights_endpoint():
     return search_flights()
 
 
-@app.route('/cities', methods=['GET'])
+@app.route('/api/cities', methods=['GET'])
 def city_endpoint():
     return search_city()
 
 
-@app.route('/airports', methods=['GET'])
+@app.route('/api/airports', methods=['GET'])
 def airports_endpoint():
     return search_airports()
 
 
-@app.route('/travel_agent', methods=['POST'])
+@app.route('/api/travel_agent', methods=['POST'])
 def travel_agent_endpoint():
     return create_flight_agent()
+
 
 
 @app.route('/login', methods=['POST'])
 def login_endpoint():
     return login()
 
-@app.route('/', methods=['GET'])
+
+@app.route('/api/', methods=['GET'])
 def home():
     return 'Hello, World!'
 
